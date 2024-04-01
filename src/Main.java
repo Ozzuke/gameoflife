1780349619aeddced3ac59e223aa7ce76aec933c
Leo-Martin Pala <57061002+Leo-Martin-Pala@users.noreply.github.com>
2024-04-01 13:33:06 +0300

    Merge remote-tracking branch 'origin/main'

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
+/*
+You could use cursor up to delete a line, and erase text, or simply overwrite with the old text with new text.
+
+int count = 1;
+System.out.print(String.format("\033[%dA",count)); // Move up
+System.out.print("\033[2K"); // Erase line content
+or clear screen
+
+System.out.print(String.format("\033[2J"));
+This is standard, but according to wikipedia the Windows console don't follow it.
+
+Have a look: http://www.termsys.demon.co.uk/vtansi.htm
+ */
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("eloo");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.print("hello");
+        Thread.sleep(1000); // Just to give the user a chance to see "hello".
+        System.out.print("\b\b\b\b\b");
+        System.out.print("world");
     }
 }
