c36f65a924ad86226eadb87d9b19316b64b6a56e
Leo-Martin Pala <57061002+Leo-Martin-Pala@users.noreply.github.com>
2024-04-04 15:12:59 +0300

    first test fix

--- src/test/MapTests.java
+++ src/test/MapTests.java
@@ -3,12 +3,14 @@ package test;
 import main.Map;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 public class MapTests {
     @Test
     public void isAlive(){
         Map map = new Map(2, 2);
         boolean a = map.isAlive(2,2);
-        Assertions.assertEquals(true, a);
+        Assertions.assertEquals(false, a);
+
     }
 }
---
+++ src/test/Mocking.java
@@ -0,0 +1,10 @@
+package test;
+
+import main.Map;
+
+public class Mocking {
+    Map map;
+    public void setMap(Map map) {
+        this.map = map;
+    }
+}
