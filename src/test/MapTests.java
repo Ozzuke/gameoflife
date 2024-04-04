616bc3437a5d76c1f9b259d46d0e65f868ecd8d9
Leo-Martin Pala <57061002+Leo-Martin-Pala@users.noreply.github.com>
2024-04-04 15:01:58 +0300

    first test

--- src/test/MapTests.java
+++ src/test/MapTests.java
@@ -1,11 +1,14 @@
 package test;
 
-import org.junit.Test;
+import main.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class MapTests {
     @Test
     public void isAlive(){
-        Map map = new Map();
-
+        Map map = new Map(2, 2);
+        boolean a = map.isAlive(2,2);
+        Assertions.assertEquals(true, a);
     }
 }
